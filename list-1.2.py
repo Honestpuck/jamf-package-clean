a71b0a76114e8be605fb9c46e058c06f1ab82aa4
WILLIAMS, Anthony <Anthony.WILLIAMS@suncorp.com.au>
2020-06-16 12:43:40 +1000

    Fixed Autopkg prefs path (Issue #2)

--- list-1.2.py
+++ list-1.2.py
@@ -17,92 +17,91 @@ import xml.etree.ElementTree as ET
 # prestage. (These change so rarely and are only accessible
 # via the new API)
 used = [
-    '484',  # DEPNotify-Suncorp-DEP-JC-1.9.0.pkg
+    "484",  # DEPNotify-Suncorp-DEP-JC-1.9.0.pkg
 ]
 
 # Which pref format to use, autopkg or jss_importer
 autopkg = False
 if autopkg:
-    plist = path.expanduser(
-        '~/Library/Preferences/com.github.autopkg..plist')
-    fp = open(plist, 'rb')
+    plist = path.expanduser("~/Library/Preferences/com.github.autopkg.plist")
+    fp = open(plist, "rb")
     prefs = plistlib.load(fp)
-    base = prefs['JSS_URL'] + '/JSSResource/'
-    auth = (prefs['API_USERNAME'], prefs['API_PASSWORD'])
+    base = prefs["JSS_URL"] + "/JSSResource/"
+    auth = (prefs["API_USERNAME"], prefs["API_PASSWORD"])
 else:
-    plist = path.expanduser('~/Library/Preferences/JPCImporter.plist')
-    fp = open(plist, 'rb')
+    plist = path.expanduser("~/Library/Preferences/JPCImporter.plist")
+    fp = open(plist, "rb")
     prefs = plistlib.load(fp)
-    base = prefs['url'] + '/JSSResource/'
-    auth = (prefs['user'], prefs['password'])
-hdrs = {'Accept': 'application/json'}
+    base = prefs["url"] + "/JSSResource/"
+    auth = (prefs["user"], prefs["password"])
+hdrs = {"Accept": "application/json"}
 
-url = base + 'patchpolicies'
+url = base + "patchpolicies"
 ret = requests.get(url, auth=auth, headers=hdrs)
 if ret.status_code != 200:
     print("Failed to get patch policies")
     exit(1)
-patchpolicies = ret.json()['patch_policies']
+patchpolicies = ret.json()["patch_policies"]
 
-url = base + 'patchsoftwaretitles'
+url = base + "patchsoftwaretitles"
 ret = requests.get(url, auth=auth, headers=hdrs)
 if ret.status_code != 200:
     print("Failed to get patchsoftwaretitles")
     exit(1)
-patchsoftwaretitles = ret.json()['patch_software_titles']
+patchsoftwaretitles = ret.json()["patch_software_titles"]
 
-url = base + 'policies'
+url = base + "policies"
 ret = requests.get(url, auth=auth, headers=hdrs)
 if ret.status_code != 200:
     print("Failed to get patchsoftwaretitles")
     exit(1)
-policies = ret.json()['policies']
+policies = ret.json()["policies"]
 
 for title in patchsoftwaretitles:
-    url = base + 'patchsoftwaretitles/id/' + str(title['id'])
+    url = base + "patchsoftwaretitles/id/" + str(title["id"])
     # we get XML due to API bug :(
     ret = requests.get(url, auth=auth)
     if ret.status_code != 200:
-        print("Failed to get title for %s:" % title['name'])
+        print("Failed to get title for %s:" % title["name"])
         continue
     root = ET.fromstring(ret.text)
-    print(root.findtext('name'), file=sys.stderr)
+    print(root.findtext("name"), file=sys.stderr)
     # get the patches for this title
-    url = base + 'patchpolicies/softwaretitleconfig/id/' + str(title['id'])
+    url = base + "patchpolicies/softwaretitleconfig/id/" + str(title["id"])
     ret = requests.get(url, auth=auth, headers=hdrs)
     if ret.status_code != 200:
-        print("Failed to get patchpolicies for %s:" % title['name'])
+        print("Failed to get patchpolicies for %s:" % title["name"])
         continue
-    for patch in ret.json()['patch policies']:
-        url = base + 'patchpolicies/id/' + str(patch['id'])
+    for patch in ret.json()["patch policies"]:
+        url = base + "patchpolicies/id/" + str(patch["id"])
         this = requests.get(url, auth=auth, headers=hdrs)
-        target = this.json()['patch_policy']['general']['target_version']
+        target = this.json()["patch_policy"]["general"]["target_version"]
         # now find the package for that version
-        for v in root.findall('versions/version'):
-            if v.findtext('software_version') == target:
+        for v in root.findall("versions/version"):
+            if v.findtext("software_version") == target:
                 try:
-                    used.append(int(v.findtext('package/id')))
+                    used.append(int(v.findtext("package/id")))
                 except:
                     False
                 continue
 
 for policy in policies:
-    url = base + 'policies/id/' + str(policy['id'])
+    url = base + "policies/id/" + str(policy["id"])
     ret = requests.get(url, auth=auth, headers=hdrs)
     if ret.status_code != 200:
-        print("Failed to get policy %s %s", (policy['id'], policy['name']))
+        print("Failed to get policy %s %s", (policy["id"], policy["name"]))
         exit(1)
-    packages_used = ret.json()['policy']['package_configuration']['packages']
+    packages_used = ret.json()["policy"]["package_configuration"]["packages"]
     for package in packages_used:
-        used.append(package['id'])
+        used.append(package["id"])
 
-url = base + 'packages'
+url = base + "packages"
 ret = requests.get(url, auth=auth, headers=hdrs)
 if ret.status_code != 200:
     print("Failed to get packages")
     exit(1)
-packages = ret.json()['packages']
+packages = ret.json()["packages"]
 
 for package in packages:
-    if not package['id'] in used:
-        print("%s\t%s" % (package['id'], package['name']))
+    if not package["id"] in used:
+        print("%s\t%s" % (package["id"], package["name"]))
--- remove-1.0.py
+++ remove-1.0.py
@@ -13,38 +13,38 @@ import xml.etree.ElementTree as ET
 # Which pref format to use, autopkg or jss_importer
 autopkg = False
 if autopkg:
-    plist = path.expanduser(
-        '~/Library/Preferences/com.github.autopkg..plist')
-    fp = open(plist, 'rb')
+    plist = path.expanduser("~/Library/Preferences/com.github.autopkg.plist")
+    fp = open(plist, "rb")
     prefs = plistlib.load(fp)
-    base = prefs['JSS_URL'] + '/JSSResource/'
-    auth = (prefs['API_USERNAME'], prefs['API_PASSWORD'])
+    base = prefs["JSS_URL"] + "/JSSResource/"
+    auth = (prefs["API_USERNAME"], prefs["API_PASSWORD"])
 else:
-    plist = path.expanduser('~/Library/Preferences/JPCImporter.plist')
-    fp = open(plist, 'rb')
+    plist = path.expanduser("~/Library/Preferences/JPCImporter.plist")
+    fp = open(plist, "rb")
     prefs = plistlib.load(fp)
-    base = prefs['url'] + '/JSSResource/'
-    auth = (prefs['user'], prefs['password'])
-hdrs = {'Accept': 'application/json'}
+    base = prefs["url"] + "/JSSResource/"
+    auth = (prefs["user"], prefs["password"])
+hdrs = {"Accept": "application/json"}
 
 for line in sys.stdin:
-    print('Doing %s' % line)
-    parts = line.rstrip().split('\t')
-    url = base + 'packages/id/' + parts[0]
+    print("Doing %s" % line)
+    parts = line.rstrip().split("\t")
+    url = base + "packages/id/" + parts[0]
     # sanity check
     ret = requests.get(url, auth=auth, headers=hdrs)
     if ret.status_code != 200:
         print("Failed to get %s" % line)
         continue
-    if ret.json()['package']['name'] != parts[1]:
-        print("Sanity 1 fail %s %s %s" %
-              (line, ret.json()['package']['name'], parts[1]))
+    if ret.json()["package"]["name"] != parts[1]:
+        print(
+            "Sanity 1 fail %s %s %s" % (line, ret.json()["package"]["name"], parts[1])
+        )
         continue
     ret = requests.delete(url, auth=auth)
     if ret.status_code != 200:
         print("Failed to delete %s" % line)
         continue
     root = ET.fromstring(ret.text)
-    if root.findtext('id') == parts[1]:
+    if root.findtext("id") == parts[1]:
         print("Failed delete sanity %s" % line)
         continue
